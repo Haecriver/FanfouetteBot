1191674b0065661edfdc61dd89df7f0e0c0f04d9
Haeriver <haecriver@outlook.fr>
2021-04-23 18:30:21 +0200

    :fire: Remove token once for all

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 /**
  * A ping pong bot, whenever you send "ping", it replies "pong".
  */
+// tslint:disable: no-submodule-imports
 
 // Import the discord.js module
 import { Client } from 'discord.js';
-import { token } from './settings.json';
+import { catFile } from './tools/catFile';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv)).argv
+
+const dtoken: string = argv.token as string || catFile(argv.tokenFile as string);
+
+if (!dtoken) {
+    console.error('Fatal error : No token provided !')
+    process.exit();
+}
 
 // Create an instance of a Discord client
 const client = new Client();
@@ -27,5 +39,10 @@ client.on('message', message => {
     }
 });
 
-// Log our bot in using the token from https://discord.com/developers/applications
-client.login(token);
+try {
+    // Log our bot in using the token from https://discord.com/developers/applications
+    client.login(dtoken);
+} catch (error) {
+    console.error(error);
+    process.exit();
+}
---
+++ src/tools/catFile.ts
@@ -0,0 +1,10 @@
+
+import fs from 'fs';
+import path from 'path';
+
+const catFile = (fileName: string) => {
+    return fileName ? fs.readFileSync(path.resolve(fileName), 'utf8') : null;
+}
+
+export { catFile };
+
