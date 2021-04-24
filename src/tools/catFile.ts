4b57e84bf212b0a335d90c2d7d2ecf8bb461002f
Haeriver <haecriver@outlook.fr>
2021-04-24 13:17:46 +0200

    :bug: Fix white blank space on tokenFile reading

--- src/tools/catFile.ts
+++ src/tools/catFile.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 
 const catFile = (fileName: string) => {
-    return fileName ? fs.readFileSync(path.resolve(fileName), 'utf8') : null;
+    return fileName ? fs.readFileSync(path.resolve(fileName), 'utf8').trim() : null;
 }
 
 export { catFile };
